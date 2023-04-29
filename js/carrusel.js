99779b04fb99a4270bd919db2a977b3574fe1d54
jscubides <zzynox2017@gmail.com>
2023-04-29 15:01:50 -0500

    se sube todo el contenido

---
+++ js/carrusel.js
@@ -0,0 +1,30 @@
+let imagenes = [
+    {
+        img: "feliz_cumpleaños",
+        title: "¡¡Feliz Cumpleaños!!",
+        fecha: "17 ABR 2023 🥳🎂🖤",
+        description: "Mi AMADA, hoy es un día especial, puede que este día hallas vivido muchas cosas, te hallan llegado diferentes sorpresas o llegaran, pero con todo ello deseo decirte algo Valentina… no tengas miedo a la vida, no tengas miedo a seguir adelante, la muerte no es la solución a los problemas, tú si PUEDES alcanzar tus objetivos. (💜 Tienes una BELLEZA ÚNICA!!! 💜)",
+        src: "CumpleañosFeliz-HappyBirthdayToYou"
+    },
+    {
+        img: "Card_1-02",
+        title: "Mujer Sabia",
+        fecha: "18 ABR 2023",
+        description: "Eres una mujer virtuosa, que sabes Escuchar los pensamientos de las personas; tú que aprecias las rarezas que otros no pueden percivir, no permitas que tus oídos se inunden de desgracias, por el contrario aliméntalos de Esperanza.",
+        src: "JuanLuisGuerra-MiBendicion"
+    },
+    {
+        img: "Card_2-02",
+        title: "Mujer Esforzada",
+        fecha: "19 ABR 2023",
+        description: "Tus Esfuerzos no han sido en vano; las situaciones han sido complicadas, pero esto no ha de definir quién eres... «He aquí que yo hago cosa nueva; pronto saldrá a luz; ¿no la conoceréis? Otra vez abriré camino en el desierto, y ríos en la soledad.» (Isaías 43:19)",
+        src: "MarcAnthony-FlorPálida"
+    },
+    {
+        img: "Card_3-02",
+        title: "Mujer Valiente",
+        fecha: "20 ABR 2023",
+        description: "No es fácil salirse de la norma, de cuestionar y examinar las estructuras rígidas que la misma sociedad ha querido imponer, la esclavitud está a flor de piel, pero se requiere Valentía para poder enfrentar nuestras propias debilidades... Nunca te rindas, ni permitas que las opiniones de los demás te definan.",
+        src: "DracoRosaftEnriqueBunbury-ObradeArte"
+    },
+]
---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Cumpleaños Feliz",
+    artist: "YouTube",
+    img: "feliz_cumpleaños_valentina-01",
+    src: "CumpleañosFeliz-HappyBirthdayToYou"
+  },
+  {
+    name: "Mi Bendición",
+    artist: "Juan Luis Guerra",
+    img: "pajaros_",
+    src: "JuanLuisGuerra-MiBendicion"
+  },
+  {
+    name: "Flor Pálida",
+    artist: "Marc Anthony",
+    img: "principito",
+    src: "MarcAnthony-FlorPálida"
+  },
+  {
+    name: "Derroche de amor",
+    artist: "Alex Campos",
+    img: "lion",
+    src: "AlexCampos-Derrochedeamor"
+  },
+  {
+    name: "Obra de Arte",
+    artist: "Draco Rosa ft Enrique Bunbury",
+    img: "astronaut_1",
+    src: "DracoRosaftEnriqueBunbury-ObradeArte"
+  },
+  {
+    name: "CASAS",
+    artist: "Rubel",
+    img: "leopardo_1",
+    src: "Rubel-CASAS"
+  },
+  {
+    name: "Quiero Vivir",
+    artist: "Draco Rosa",
+    img: "mariposa",
+    src: "DracoRosa-QuieroVivir"
+  },
+  {
+    name: "Amor Exagerado",
+    artist: "González y Gilberto Daza",
+    img: "pez_1",
+    src: "DomínicoGonzálezFeatGilbertoDaza-AmorExagerado"
+  },
+  {
+    name: "What a Beautiful Name",
+    artist: "Hillsong Worship",
+    img: "venado_",
+    src: "HillsongWorship-WhatABeautifulName"
+  },
+  {
+    name: "Se Quedó Conmigo",
+    artist: "Jesús Adrián Romero",
+    img: "tigre_1",
+    src: "JesúsAdriánRomero-SeQuedóConmigo"
+  },
+  {
+    name: "Jesucristo Basta",
+    artist: "Un Corazón feat Living",
+    img: "zorro_2",
+    src: "UnCorazónfeatLiving-JesucristoBasta"
+  },
+  {
+    name: "Aquí Estas",
+    artist: "Way Maker",
+    img: "pada_1",
+    src: "WayMaker-AquíEstas"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
